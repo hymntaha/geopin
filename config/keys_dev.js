de53487d0f1215e192877cb77ed8f4dee584f230
taha uygun <tahauygun@gmail.com>
2019-06-24 03:38:43 -0700

    fixing some google stuff

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,3 +1,4 @@
 module.exports = {
     mongoURI : 'mongodb+srv://taha:taha1985@geopins-v8atw.mongodb.net/test?retryWrites=true&w=majority',
+    OAUTH_CLIENT_ID:'300849443661-ms7k1kn107ac6hu30kpdo5alhpavsifm.apps.googleusercontent.com'
 };
