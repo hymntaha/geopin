3c0c8c21c55bd1e17b811b89cc905dfd01dd7a36
taha uygun <tahauygun@gmail.com>
2019-06-24 02:53:42 -0700

    setting up db

---
+++ config/keys_dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://taha:taha1985@geopins-v8atw.mongodb.net/test?retryWrites=true&w=majority',
+};
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const {ApolloServer} = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./typeDefs');
-const resolvers =  require('./resolvers');
+const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true },
+  )
+  .then(() => console.log('DB connected!'))
+  .catch(err => console.log(err));
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
-
-server.listen().then(({url})=>{
-  console.log(`Server listening on ${url}`);
+  resolvers,
 });
 
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});
