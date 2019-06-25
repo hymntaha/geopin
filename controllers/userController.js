49a41ec5eac9d8d5c0c38e1369175ee64644b8de
taha uygun <tahauygun@gmail.com>
2019-06-25 00:39:52 -0700

    finishing authentication

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+const { OAuth2Client } = require('google-auth-library');
+const oathkey = require('./config/keys_dev').OATH_CLIENT_ID;
+
+const client = new OAuth2Client(oathkey);
+
+exports.findOrCreateUser = async token => {
+  const googleUser = await verifyAuthToken(token);
+
+  const user = await checkIfUserExists(googleUser.email);
+
+  return user ? user : createNewUser(googleUser);
+};
+
+const verifyAuthToken = async token => {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: oathkey,
+    });
+    return ticket.getPayload();
+  } catch (err) {
+    console.error('Error verifying auth token', err);
+  }
+};
+
+const checkIfUserExists = async email => await User.findOne({ email }).exec();
+const createNewUser = googleUser => {
+  const { name, email, picture } = googleUser;
+  const user = { name, email, picture };
+  return new User(user).save();
+};
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
 const mongoose = require('mongoose');
 
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+const {findOrCreateUser} = require('./controllers/userController');
 const db = require('./config/keys_dev').mongoURI;
+
 
 mongoose
   .connect(
@@ -17,6 +20,19 @@ mongoose
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({req}) => {
+    let authToken = null
+    let currentUser = null
+    try{
+      authToken = req.headers.authentication
+      if (authToken) {
+       currentUser = await findOrCreateUser(authToken)
+      }
+    } catch (err){
+      console.error(`Unable to authenticate user with token ${authToken}`)
+    }
+    return { currentUser }
+  }
 });
 
 server.listen().then(({ url }) => {
