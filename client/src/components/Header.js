57fb2d310b77f1422a28ee1bc9d5bd05ff51a94e
taha uygun <tahauygun@gmail.com>
2019-07-21 02:24:15 -0700

    working on header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,36 +1,41 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React, { useContext } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 // import AppBar from "@material-ui/core/AppBar";
 // import Toolbar from "@material-ui/core/Toolbar";
 // import Map from "@material-ui/icons/Map";
 // import Typography from "@material-ui/core/Typography";
 
+import Context from '../context';
+
 const Header = ({ classes }) => {
+  const { state } = useContext(Context);
+  const { currentUser } = state;
+
   return <div>Header</div>;
 };
 
 const styles = theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   grow: {
     flexGrow: 1,
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   },
   icon: {
     marginRight: theme.spacing.unit,
-    color: "green",
-    fontSize: 45
+    color: 'green',
+    fontSize: 45,
   },
   mobile: {
-    display: "none"
+    display: 'none',
   },
   picture: {
-    height: "50px",
-    borderRadius: "90%",
-    marginRight: theme.spacing.unit * 2
-  }
+    height: '50px',
+    borderRadius: '90%',
+    marginRight: theme.spacing.unit * 2,
+  },
 });
 
 export default withStyles(styles)(Header);
