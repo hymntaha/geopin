b35cbb7ba369c47de21f9ba1d573fdd09e34ec28
taha uygun <tahauygun@gmail.com>
2019-07-21 21:58:04 -0700

    first map work

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,9 +1,16 @@
-import React from "react";
-import withRoot from "../withRoot";
+import React from 'react';
 import Header from '../components/Header';
+import Map from '../components/Map';
+
+import withRoot from '../withRoot';
 
 const App = () => {
-  return <div><Header/></div>;
+  return (
+    <div>
+      <Header />
+      <Map />
+    </div>
+  );
 };
 
 export default withRoot(App);
