05c13397aa7cc9bf7d758570b26e13aa78e9fc3c
taha uygun <tahauygun@gmail.com>
2019-07-25 23:56:19 -0700

    create comments fix

--- client/src/components/Comment/CreateComment.js
+++ client/src/components/Comment/CreateComment.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { withStyles } from "@material-ui/core";
-// import InputBase from "@material-ui/core/InputBase";
-// import IconButton from "@material-ui/core/IconButton";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SendIcon from "@material-ui/icons/Send";
-// import Divider from "@material-ui/core/Divider";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import ClearIcon from "@material-ui/icons/Clear";
+import SendIcon from "@material-ui/icons/Send";
+import Divider from "@material-ui/core/Divider";
 
 const CreateComment = ({ classes }) => {
   return <div>CreateComment</div>;
