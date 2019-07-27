e56bb32fce86cef0eda967320dff13707ffc4f45
taha uygun <tahauygun@gmail.com>
2019-07-26 22:19:42 -0700

    finishing comment section

--- client/src/components/Comment/Comments.js
+++ client/src/components/Comment/Comments.js
@@ -7,7 +7,20 @@ import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
 
-const Comments = ({ classes }) => <div>Comments</div>;
+const Comments = ({ comments, classes }) => (
+  <List> {comments.map((comment,i)=> (
+    <ListItem key={i} alignItems = "flex-start">
+      <ListItemAvatar>
+        <Avatar src={comment.author.picture} alt={comment.author.name} />
+      </ListItemAvatar>
+      <ListItemText primary={comment.text} secondary={
+        <Typography className={classes.inline} component="span" color="textPrimary">
+          {comment.author.name}
+        </Typography>
+      }/>
+    </ListItem>
+  ))}</List>
+);
 
 const styles = theme => ({
   root: {
